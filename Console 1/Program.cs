b87075d1b16f5670eacfa5d13391357cb2b2a3ad
Lis <Lis@LENOVO>
2017-10-26 20:46:01 +0300

    Перемога

---
+++ Console 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This program lists all the files in the directory:");
+            Console.WriteLine("Ura:");
+            string sName = Console.ReadLine();
+            Console.WriteLine("Привет, " + sName);
+            Console.WriteLine("Нажмите ENTER что бы выйти из программы");
+            Console.Read();
+
+        }
+    
+    }
+}
