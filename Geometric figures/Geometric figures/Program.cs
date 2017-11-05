b838e01ba1bb9732891bf4a88318365b2f15ef9b
LisovetsUra1984 <33056492+LisovetsUra1984@users.noreply.github.com>
2017-11-05 16:55:21 +0200

    geometric figures

---
+++ Geometric figures/Geometric figures/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometric_figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle");
+
+            for (int i = 1; i < 7; i++)
+            {
+                string str = "";
+                for (int j = 0; j < i; j++)
+                {
+                    str += "* ";
+                }
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("");
+
+            
+            Console.WriteLine("Square");
+            for (int i = 0; i < 9; i++)
+            {
+                string str = "";
+                for (int j = 0; j < 8; j++)
+                {
+                    str += "* ";
+                }
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("");
+
+        }
+    }
+}
